a993e51735582595f0c4eecf2cd7e6b31a44ed9e
Nhan121631 <phamphunhanpham8@gmail.com>
2026-01-23 16:01:08 +0700

    feat: Add H2 database dependency and test properties for in-memory testing

--- job/src/test/java/com/vku/job/JobApplicationTests.java
+++ job/src/test/java/com/vku/job/JobApplicationTests.java
@@ -2,8 +2,17 @@ package com.vku.job;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class JobApplicationTests {
 
 	@Test
