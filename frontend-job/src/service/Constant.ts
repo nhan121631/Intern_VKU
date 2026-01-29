fdf329292689dca55c62c3e8ffcdb64c84fa2bda
Nhan121631 <phamphunhanpham8@gmail.com>
2026-01-29 13:29:14 +0700

    fix: Revert API and IMAGE URLs to local development endpoints

--- frontend-job/src/service/Constant.ts
+++ frontend-job/src/service/Constant.ts
@@ -1,4 +1,4 @@
-// export const API_URL = "http://localhost:8080/api"
-// export const IMAGE_URL = "http://localhost:8080" 
-export const API_URL = "https://nhantasks.duckdns.org/api"
-export const IMAGE_URL = "https://nhantasks.duckdns.org"
+export const API_URL = "http://localhost:8080/api"
+export const IMAGE_URL = "http://localhost:8080" 
+// export const API_URL = "https://nhantasks.duckdns.org/api"
+// export const IMAGE_URL = "https://nhantasks.duckdns.org"
